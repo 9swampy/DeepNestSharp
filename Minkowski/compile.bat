bf9c6c588d3c477359230b1aee1e61b2cf352bd8
9swampy <github@prims.com>
2021-06-17 00:13:48 +0100

    Simplify compile.bat and recompile generates a DLL I can drop in and run as Debug/Any CPU seemingly fine.

--- Minkowski/compile.bat
+++ Minkowski/compile.bat
@@ -1,1 +1,2 @@
-cl /Ox /Ob2 /Oi /Ot /Oy- /GL /GF /Gm- /MD /GS- /arch:AVX2 /fp:fast -I <boost_1.62_path> /LD minkowski.cc
+@rem cl /Ox /Ob2 /Oi /Ot /Oy- /GL /GF /Gm- /MD /GS- /arch:AVX2 /fp:fast -I "C:\boost_1_62" /LD minkowski.cc
+cl /Ox -I "C:\boost_1_62" /LD minkowski.cc
