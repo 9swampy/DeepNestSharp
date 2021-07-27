102fa785338ecd1da0773ee8348d069f3f27c2cf
9swampy <github@prims.com>
2021-07-27 13:58:54 +0100

    MinkowskiDlls.

--- Minkowski/compile.bat
+++ Minkowski/compile.bat
@@ -1,4 +1,4 @@
-cl /Ox /Ob2 /Oi /Ot /Oy- /GL /GF /Gm- /MD /GS- /arch:AVX2 /fp:fast -I "D:\boost\boost_1_62_0" /LD minkowski.cc
+@REM cl /Ox /Ob2 /Oi /Ot /Oy- /GL /GF /Gm- /MD /GS- /arch:AVX2 /fp:fast -I "D:\boost\boost_1_62_0" /LD minkowski.cc
 @REM cl /Ox -I "D:\boost\boost_1_62_0" /LD minkowski.cc
-
+cl /Ox -I "D:\boost\boost_1_76_0" /LD minkowski.cc
 @REM /platform:x86 https://www.codeproject.com/Articles/552348/Csharp-C-Interop-with-DllImport
